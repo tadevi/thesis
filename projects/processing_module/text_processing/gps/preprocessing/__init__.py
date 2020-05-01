78db5e74aaaecbc2f93c16a923b45e6a16cf2e5e
Tuan Kiet <tuankiet.luong@foodycorp.vn>
2020-05-01 10:33:45 +0700

    Init template project

---
+++ projects/processing_module/text_processing/gps/preprocessing/__init__.py
@@ -0,0 +1,15 @@
+# Validate GPS data
+# Remove unexpected value such as negative longtitude, lattitude, velocity
+# Format data to the specified format to send to other modules
+#
+# This module should have def run(input, configs)
+# @input: received data from protocol_abstraction with json format.
+# @configs: addition configs
+# Using import json to decode json input to python dictionary
+
+def init(configs):
+    pass
+
+
+def run(input, configs):
+    pass
