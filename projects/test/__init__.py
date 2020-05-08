ad9b115580da11581089b0da73068e64d8a79758
anh2003maa <thanductaimgt@gmail.com>
2020-05-08 19:08:29 +0700

    implement face extraction and face recognition; modify human detection and more

--- projects/test/__init__.py
+++ projects/test/__init__.py
@@ -1,5 +1,13 @@
+import os
+
+parent_folder_path = os.path.abspath(os.path.dirname(__file__))
+data_folder_path = os.path.join(parent_folder_path, "data")
+
+
 def run():
-    test_human_detection()
+    # test_validate_and_storage()
+    # test_human_detection()
+    test_camera_flow()
 
 
 def test_validate_and_storage():
@@ -50,21 +58,6 @@ def test_validate_and_storage():
 def test_human_detection():
     from modules.image import human_detection
     import cv2
-    import os
-
-    # parent_folder_path = os.path.abspath(os.path.dirname(__file__))
-    # parent_folder_path = os.path.join(parent_folder_path, "data")
-
-    # human_path1 = os.path.join(parent_folder_path, "human1.jpg")
-    # human_path2 = os.path.join(parent_folder_path, "human2.jpg")
-    # no_human_path1 = os.path.join(parent_folder_path, "no_human1.jpg")
-    # no_human_path2 = os.path.join(parent_folder_path, "no_human2.jpg")
-
-    # 1280 x 720 resolution
-    # human_img1 = cv2.imread(human_path1)
-    # human_img2 = cv2.imread(human_path2)
-    # no_human_img1 = cv2.imread(no_human_path1)
-    # no_human_img2 = cv2.imread(no_human_path2)
 
     human_detection = human_detection.Main({})
 
@@ -74,10 +67,37 @@ def test_human_detection():
     ret, input = cap.read()
 
     while input is not None:
-        # human_detection.run(human_img1)
-        # human_detection.run(human_img2)
-        # human_detection.run(no_human_img1)
-        # human_detection.run(no_human_img2)
-
         human_detection.run(input)
+        ret, input = cap.read()
+
+
+def test_camera_flow():
+    import modules.image.human_detection as human_detection_pkg
+    import modules.image.face_recognition as face_recognition_pkg
+    import modules.image.face_extraction as face_extraction_pkg
+    import cv2
+
+    human_detection_module = human_detection_pkg.Main({})
+    face_extraction_module = face_extraction_pkg.Main({})
+    face_recognition_module = face_recognition_pkg.Main({})
+
+    cap = cv2.VideoCapture(0)
+    ret, input = cap.read()
+
+    while input is not None:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        input = cv2.resize(input, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        input = input[:, :, ::-1]
+
+        if human_detection_module.run(input):
+            face_locations = face_extraction_module.run(input)
+            if len(face_locations) > 0:
+                face_recognition_input = {
+                    "frame": input,
+                    "face_locations": face_locations
+                }
+                face_recognition_module.run(face_recognition_input)
+
         ret, input = cap.read()
